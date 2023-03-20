8de3341730c4eb962d952edc0bd8d494fccb6d57
HatsoPadsor <112861430+ShadowDragon2040@users.noreply.github.com>
2023-03-20 12:02:04 +0100

    kepek

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,11 +2,11 @@ import ImageSlider from "./ImageSlider";
 import containerStyles from "./GalleryElements";
 const Gallery = () => {
   const slides = [
-    { url: "http://localhost:3000/image-1.jpg", title: "beach" },
-    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
-    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
-    { url: "http://localhost:3000/image-4.jpg", title: "city" },
-    { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+    { url: "https://magyarmezogazdasag.hu/sites/default/files/styles/default_crop/public/uploads/2022-09//the-lucky-neko-uepn9yctcy0-unsplash_0.jpg?itok=QqQMQsX7", title: "beach" },
+    { url: "https://cms.static.marquardmedia.hu/data/cikk/224/224688.1200x630.jpg", title: "boat" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVsnD26Hx4k9gcVRxHubbTDMgeBPKrIMzx6w&usqp=CAU", title: "forest" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRblntF9MWtb-CMuaRLimkesv0L1_lQrzOYdQ&usqp=CAU", title: "city" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflZRYU-MSepyrreZnGFX-G7pZ2mtGrnfH5w&usqp=CAU", title: "italy" },
   ];
 
   return (
