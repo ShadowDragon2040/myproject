01f70244daaed1f9942692236a69cecf2d83632c
pejkob <pejkob@kkszki.hu>
2023-11-28 10:37:14 +0100

    SlideShow.js initial

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 import Sidebar from '../components/Sidebar';
 import SlideShow from '../components/SlideShow';
+import { InfoContainer, InfoRow } from '../components/TextElements';
 
 function ShopPage(){
 
@@ -14,14 +15,16 @@ function ShopPage(){
   }
     return(
       <>
-      <Sidebar isOpen={isOpen} toggle={toggle}/>
-      <Navbar toggle={toggle}/>
-
-      <SlideShow/>
-
+      <Navbar/>
+      <InfoContainer>
+        <InfoRow>
+          <p>asdsadsd</p>
+        </InfoRow>
+      </InfoContainer>
       <Footer/>
       </>
-    )
+      
+        )
 }
 
 export default ShopPage
